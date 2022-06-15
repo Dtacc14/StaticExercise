2f7a96a69ac9a6eaad9a098b2db4b37a77156bab
David Taccetta <taccetta14@gmail.com>
2022-06-15 11:25:52 -0400

    Initial StaticExercise Commmit

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TempConverter.Fahrenheit = 59;
+            Console.WriteLine(TempConverter.FahrenheitToCelsius());
+            Console.WriteLine();
+
+            TempConverter.Celcius = 15;
+            Console.WriteLine(TempConverter.CelsiusToFahrenheit());
+        }
+    }
+}
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        public static double Fahrenheit { get; set; }
+        public static double Celcius { get; set; }
+        public static double FahrenheitToCelsius()
+        {
+            double fahrenheit = Fahrenheit;
+            double celsius = (fahrenheit - 32) / 1.8;
+            return celsius;
+        }
+        public static double CelsiusToFahrenheit()
+        {
+            double celcius = Celcius;
+            double fahrenheit = (celcius * 1.8) + 32;
+            return fahrenheit;
+        }
+    }
+}
+
